9032f5880c7f3f04693c3baaaa4684ef484cc0e4
miguelrod21 <55973986+miguelrod21@users.noreply.github.com>
2019-11-01 19:37:37 +0100

    tail funciona

--- Practica1/tail.c
+++ Practica1/tail.c
@@ -1,3 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 int tail(int n1){
-	return n1+4;
+	int cont=0;
+	int numLineas=0;
+	char buffer [1024];
+	char ** lineas = (char **) malloc (n1*sizeof(char*));
+	for(int i=0;i<=n1;i++){
+		lineas[i] = (char*) malloc (sizeof(char)*1024);
+	}
+
+	while(fgets(buffer,1024,stdin)!=NULL){
+		if(cont<n1){
+			strcpy(lineas[cont],buffer);
+			cont++;
+		}else{
+			for(int i=0;i<n1;i++){
+				strcpy(lineas[i],lineas[i+1]);
+			}
+		strcpy(lineas[cont],buffer);
+		}
+		
+	}
+	for(int k=0;k<n1+1;k++){
+		printf("%s",*(lineas+k));
+	}
+	free(lineas);
 }
