d5f93dd33a7483639df8e1a734e7cbbdc7855f41
miguelrod21 <55973986+miguelrod21@users.noreply.github.com>
2019-12-05 11:07:39 +0100

    cd errores bien

--- MiniShell/test.c
+++ MiniShell/test.c
@@ -25,7 +25,7 @@ int main(void) {
 		if (line==NULL) {
 			continue;
 		}
-           if (strcmp(line -> commands[0].argv[0], "cd") == 0){
+           if (strcmp(line -> commands[0].argv[0], "cd") == 0 && line->ncommands==1){
                 if (line -> commands[0].argc == 1){
                     chdir(getenv("HOME"));                   
                 }else{
@@ -77,46 +77,26 @@ int main(void) {
 					wait(NULL);
 				}
 		}else if(line->ncommands == 2){ //2 comandos entrada + salida------------------------------------------------------------------------------
-			pipe(p);
-			pid =fork();
-			if(pid<0){
-				printf("Ha fallado el fork");
-				return 1;
-			}else if(pid==0){ //codigo del hijo1
-				if (line->redirect_input != NULL) {
-					printf("redirección de entrada: %s\n", line->redirect_input);
-					entrada = open(line->redirect_input,O_RDONLY);
-					if(entrada!=-1){
-						dup2(entrada,0);
-					}else{
-						fprintf(stderr, "%s: Error %d.\nError al leer fichero de entrada. \n",line->redirect_input,errno);
-						exit(0);
-					}
-				}		
-				if(line->redirect_error != NULL){
-					printf("redirección de error: %s\n", line->redirect_error);
-					error = open(line->redirect_error,O_RDONLY);
-					if(error==-1){
-						fprintf(stderr, "Error: %s. Error en la redirección del fichero\n", line->redirect_error);
-					}
-					dup2(error,2);
-				}		
-				close(p[0]);
-				dup2(p[1],1);
-				execv(line->commands[0].filename, line->commands[0].argv);
-			}else{ //codigo padre
-				pid = fork();
-				if(pid == 0){ //codigo hijo2
-					if (line->redirect_output != NULL) {
-						printf("redirección de salida: %s\n", line->redirect_output);
-						salida = open(line->redirect_output,O_WRONLY | O_CREAT | O_TRUNC, 0644);
-						if(salida<0){
-							fprintf(stderr, "%s: Error %d.\nError al leer fichero de salida. \n",line->redirect_output, errno);
-							exit(0);
+			if(strcmp(line->commands[0].argv[0], "cd") == 0 || strcmp(line->commands[1].argv[0], "cd") == 0){
+				printf("Error en el comando cd.\n");
+				i=line->ncommands;
+			}else{
+				pipe(p);
+				pid =fork();
+				if(pid<0){
+					printf("Ha fallado el fork");
+					return 1;
+				}else if(pid==0){ //codigo del hijo1
+					if (line->redirect_input != NULL) {
+						printf("redirección de entrada: %s\n", line->redirect_input);
+						entrada = open(line->redirect_input,O_RDONLY);
+						if(entrada!=-1){
+							dup2(entrada,0);
 						}else{
-							dup2(salida,1);
+							fprintf(stderr, "%s: Error %d.\nError al leer fichero de entrada. \n",line->redirect_input,errno);
+							exit(0);
 						}
-					}
+					}		
 					if(line->redirect_error != NULL){
 						printf("redirección de error: %s\n", line->redirect_error);
 						error = open(line->redirect_error,O_RDONLY);
@@ -124,87 +104,13 @@ int main(void) {
 							fprintf(stderr, "Error: %s. Error en la redirección del fichero\n", line->redirect_error);
 						}
 						dup2(error,2);
-					}
-					close(p[1]);	
-					dup2(p[0],0);
-					if((line->commands[i].filename)!=NULL){
-						execvp(line->commands[1].filename,line->commands[1].argv);
-					}else{
-						printf("%s: No se encuentra el mandato\n", *line->commands[i].argv);
-					}
-				}else{ //codigo padre
+					}		
 					close(p[0]);
-					close(p[1]);
-					wait(NULL);
-					wait(NULL);
-				}
-			}
-		}else{ //Más de 2 comandos
-			pipes = (int **) malloc ((line->ncommands-1) * sizeof(int *)); 
-			for(i=0;i<line->ncommands-1;i++){
-				pipes[i] = (int *) malloc (sizeof(int)*2); 
-				errpipe = pipe(pipes[i]);
-				if(errpipe<0){
-					fprintf(stderr, "Error en el pipe: %d. Error %d\n",i, errno );
-				}
-			}
-
-			for(i = 0; i< line->ncommands;i++){
-				if(strcmp(line->commands[i].argv[0], "cd") == 0){
-					printf("Error en el comando cd.\n");
-					i=line->ncommands;
-				}
-				pid = fork();
-				if(pid<0){
-					fprintf(stderr, "Ha fallado el fork. %d\n", errno);
-				}else if(pid==0){
-					if(i==0){
-						if (line->redirect_input != NULL) {
-							printf("redirección de entrada: %s\n", line->redirect_input);
-							entrada = open(line->redirect_input,O_RDONLY);
-							if(entrada!=-1){
-								dup2(entrada,0);
-							}else{
-								fprintf(stderr, "%s: Error %d.\nError al leer fichero de entrada. \n",line->redirect_input,errno);
-								exit(0);
-							}
-						}
-						if(line->redirect_error != NULL){
-							printf("redirección de error: %s\n", line->redirect_error);
-							error = open(line->redirect_error,O_RDONLY);
-							if(error==-1){
-								fprintf(stderr, "Error: %s. Error en la redirección del fichero\n", line->redirect_error);
-							}
-							dup2(error,2);
-						}		
-						for(j=i+1; j<line->ncommands-1; j++){ 
-							close(pipes[j][1]);
-							close(pipes[j][0]);
-						}
-						close (pipes[0][0]);
-                        close (pipes[1][0]);
-                        close (pipes[1][1]);
-						dup2(pipes[0][1],1);
-						
-					}else if(i>0 && i<line->ncommands-1){
-						if(i==1){
-							for(j=0; j<i-1; j++){ 
-								close(pipes[j][1]);
-								close(pipes[j][0]);	
-							}
-						}
-						if(i!=1){ 
-							for(j=0; j<i-1; j++){ 
-								close(pipes[j][1]);
-								close(pipes[j][0]);
-							}
-							
-						}
-						close(pipes[i-1][1]);
-						dup2(pipes[i-1][0],0);
-						close(pipes[i][0]);
-						dup2(pipes[i][1],1);
-					}else{
+					dup2(p[1],1);
+					execv(line->commands[0].filename, line->commands[0].argv);
+				}else{ //codigo padre
+					pid = fork();
+					if(pid == 0){ //codigo hijo2
 						if (line->redirect_output != NULL) {
 							printf("redirección de salida: %s\n", line->redirect_output);
 							salida = open(line->redirect_output,O_WRONLY | O_CREAT | O_TRUNC, 0644);
@@ -223,20 +129,120 @@ int main(void) {
 							}
 							dup2(error,2);
 						}
-						for(j=0; j<line->ncommands-2; j++){ 
-							close(pipes[j][1]);
-							close(pipes[j][0]);
+						close(p[1]);	
+						dup2(p[0],0);
+						if((line->commands[i].filename)!=NULL){
+							execvp(line->commands[1].filename,line->commands[1].argv);
+						}else{
+							printf("%s: No se encuentra el mandato\n", *line->commands[i].argv);
 						}
-						close(pipes[i-1][1]);
-						dup2(pipes[i-1][0],0);
+					}else{ //codigo padre
+						close(p[0]);
+						close(p[1]);
+						wait(NULL);
+						wait(NULL);
 					}
-					if((line->commands[i].filename)!=NULL){
-						execvp(line->commands[i].filename,line->commands[i].argv);
-					}else{
-						printf("%s: No se encuentra el mandato\n", *line->commands[i].argv);
+				}
+			}
+		}else{ //Más de 2 comandos
+			pipes = (int **) malloc ((line->ncommands-1) * sizeof(int *)); 
+			for(i=0;i<line->ncommands-1;i++){
+				pipes[i] = (int *) malloc (sizeof(int)*2); 
+				errpipe = pipe(pipes[i]);
+				if(errpipe<0){
+					fprintf(stderr, "Error en el pipe: %d. Error %d\n",i, errno );
+				}
+			}
+
+			for(i = 0; i< line->ncommands;i++){
+				if(strcmp(line->commands[i].argv[0], "cd") == 0){
+					printf("Error en el comando cd.\n");
+					i=line->ncommands;
+				}else{
+					pid = fork();
+					if(pid<0){
+						fprintf(stderr, "Ha fallado el fork. %d\n", errno);
+					}else if(pid==0){
+						if(i==0){
+							if (line->redirect_input != NULL) {
+								printf("redirección de entrada: %s\n", line->redirect_input);
+								entrada = open(line->redirect_input,O_RDONLY);
+								if(entrada!=-1){
+									dup2(entrada,0);
+								}else{
+									fprintf(stderr, "%s: Error %d.\nError al leer fichero de entrada. \n",line->redirect_input,errno);
+									exit(0);
+								}
+							}
+							if(line->redirect_error != NULL){
+								printf("redirección de error: %s\n", line->redirect_error);
+								error = open(line->redirect_error,O_RDONLY);
+								if(error==-1){
+									fprintf(stderr, "Error: %s. Error en la redirección del fichero\n", line->redirect_error);
+								}
+								dup2(error,2);
+							}		
+							for(j=i+1; j<line->ncommands-1; j++){ 
+								close(pipes[j][1]);
+								close(pipes[j][0]);
+							}
+							close (pipes[0][0]);
+	                        close (pipes[1][0]);
+	                        close (pipes[1][1]);
+							dup2(pipes[0][1],1);
+							
+						}else if(i>0 && i<line->ncommands-1){
+							if(i==1){
+								for(j=0; j<i-1; j++){ 
+									close(pipes[j][1]);
+									close(pipes[j][0]);	
+								}
+							}
+							if(i!=1){ 
+								for(j=0; j<i-1; j++){ 
+									close(pipes[j][1]);
+									close(pipes[j][0]);
+								}
+								
+							}
+							close(pipes[i-1][1]);
+							dup2(pipes[i-1][0],0);
+							close(pipes[i][0]);
+							dup2(pipes[i][1],1);
+						}else{
+							if (line->redirect_output != NULL) {
+								printf("redirección de salida: %s\n", line->redirect_output);
+								salida = open(line->redirect_output,O_WRONLY | O_CREAT | O_TRUNC, 0644);
+								if(salida<0){
+									fprintf(stderr, "%s: Error %d.\nError al leer fichero de salida. \n",line->redirect_output, errno);
+									exit(0);
+								}else{
+									dup2(salida,1);
+								}
+							}
+							if(line->redirect_error != NULL){
+								printf("redirección de error: %s\n", line->redirect_error);
+								error = open(line->redirect_error,O_RDONLY);
+								if(error==-1){
+									fprintf(stderr, "Error: %s. Error en la redirección del fichero\n", line->redirect_error);
+								}
+								dup2(error,2);
+							}
+							for(j=0; j<line->ncommands-2; j++){ 
+								close(pipes[j][1]);
+								close(pipes[j][0]);
+							}
+							close(pipes[i-1][1]);
+							dup2(pipes[i-1][0],0);
+						}
+						if((line->commands[i].filename)!=NULL){
+							execvp(line->commands[i].filename,line->commands[i].argv);
+						}else{
+							printf("%s: No se encuentra el mandato\n", *line->commands[i].argv);
+						}
+					}else{ //el padre
+						
 					}
-				}else{ //el padre
-					
 				}
 				
 			}
